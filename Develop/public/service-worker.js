c8afa98241bc2dce0abd0ccb7397654cb2e3d5ff
Leslie Codes <84750526+Lesliescodes@users.noreply.github.com>
2021-10-24 19:57:36 -0600

    cache working

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,28 +1,32 @@
 // Uncomment the lines below to set up the cache files
 
-const CACHE_NAME = 'my-site-cache-v1';
+const CACHE_NAME = 'budgetTracker-site-cache-v1';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/favicon.ico',
+  // '/favicon.ico',
   '/manifest.json',
-  '/assets/css/style.css',
-  '/assets/js/loadImages.js',
-  '/assets/images/icons/icon-72x72.png',
-  '/assets/images/icons/icon-96x96.png',
-  '/assets/images/icons/icon-128x128.png',
-  '/assets/images/icons/icon-144x144.png',
-  '/assets/images/1.jpg',
-  '/assets/images/2.jpg',
-  '/assets/images/3.jpg',
-  '/assets/images/4.jpg',
-  '/assets/images/5.jpg',
-  '/assets/images/6.jpg',
-  '/assets/images/7.jpg',
-  '/assets/images/8.jpg',
-  '/assets/images/9.jpg'
+  '/assets/css/styles.css',
+  '/api/transaction',
+  '/api/transaction/bulk',
+  '/assets/js/index.js',
+  '/assets/idb.js',
+
+  // '/assets/images/icons/icon-72x72.png',
+  // '/assets/images/icons/icon-96x96.png',
+  // '/assets/images/icons/icon-128x128.png',
+  // '/assets/images/icons/icon-144x144.png',
+  // '/assets/images/1.jpg',
+  // '/assets/images/2.jpg',
+  // '/assets/images/3.jpg',
+  // '/assets/images/4.jpg',
+  // '/assets/images/5.jpg',
+  // '/assets/images/6.jpg',
+  // '/assets/images/7.jpg',
+  // '/assets/images/8.jpg',
+  // '/assets/images/9.jpg'
 ];
 
 // Install the service worker
@@ -34,7 +38,7 @@ self.addEventListener('install', function(evt){
         return cache.addAll(FILES_TO_CACHE);
        })
    );
-   self.skipWaiting(); 
+  //  self.skipWaiting(); 
 });
 
 // Activate the service worker and remove old data from the cache
